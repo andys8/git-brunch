74b1defc3615e4009f3252b078d39e2567d5ffcc
andys8 <andys8@users.noreply.github.com>
2019-08-14 01:41:01 +0200

    Improve naming of ui components

--- src/GitBrunch.hs
+++ src/GitBrunch.hs
@@ -24,64 +24,57 @@ import           Brick.Widgets.Core                       ( hLimit
                                                           , withAttr
                                                           , withBorderStyle
                                                           , (<+>)
-                                                          , padLeftRight
+                                                          , padAll
                                                           )
 import qualified Brick.Widgets.List            as L
 import qualified Data.Vector                   as Vec
 import           Git
 import           Data.Maybe                    as Maybe
 
+
 drawUI :: (Show a) => L.List () a -> [Widget ()]
-drawUI l = [ui]
+drawUI l =
+  [ C.vCenter $ padAll 1 $ vBox
+      (C.hCenter branchBox : str " " : instructions)
+  ]
  where
-  label = str "Branch " <+> cur <+> str " of " <+> total
-  cur   = case l ^. L.listSelectedL of
-    Nothing -> str "-"
-    Just i  -> str (show (i + 1))
-  total = str $ show $ Vec.length $ l ^. L.listElementsL
-  box =
+  branchBox =
     withBorderStyle BS.unicodeBold
-      $ B.borderWithLabel label
+      $ B.borderWithLabel (str "Branch")
       $ hLimit 100
       $ L.renderList listDrawElement True l
-  ui = C.vCenter $ padLeftRight 1 $ vBox
-    [ C.hCenter box
-    , str " "
-    , instruction "HJKL/arrows" "navigate"
-    , instruction "Enter"       "checkout"
-    , instruction "Esc/Q"       "exit"
+  instructions =
+    [ drawInstruction "HJKL/arrows" "navigate"
+    , drawInstruction "Enter"       "checkout"
+    , drawInstruction "Esc/Q"       "exit"
     ]
-  instruction keys action =
-    C.hCenter
-      $   str "Press "
-      <+> withAttr "key" (str keys)
-      <+> str " to "
-      <+> withAttr "bold" (str action)
-      <+> str "."
 
+drawInstruction :: String -> String -> Widget ()
+drawInstruction keys action =
+  C.hCenter
+    $   str "Press "
+    <+> withAttr "key" (str keys)
+    <+> str " to "
+    <+> withAttr "bold" (str action)
+    <+> str "."
 
 appEvent
   :: L.List () Branch
   -> T.BrickEvent () e
   -> T.EventM () (T.Next (L.List () Branch))
 appEvent l (T.VtyEvent e) = case e of
-
   V.EvKey V.KEsc        [] -> M.halt $ L.listClear l
   V.EvKey (V.KChar 'q') [] -> M.halt $ L.listClear l
   V.EvKey V.KEnter      [] -> M.halt l
-
   ev -> M.continue =<< L.handleListEventVi L.handleListEvent ev l
 appEvent l _ = M.continue l
 
 listDrawElement :: Show a => Bool -> a -> Widget ()
 listDrawElement selected a = C.hCenter $ str (show a)
 
-initialState :: [Branch] -> L.List () Branch
-initialState branches = L.list () (Vec.fromList branches) 1
 
-
-theMap :: A.AttrMap
-theMap = A.attrMap
+attributeMap :: A.AttrMap
+attributeMap = A.attrMap
   V.defAttr
   [ (L.listAttr        , fg V.white)
   , (L.listSelectedAttr, V.black `on` V.yellow)
@@ -89,12 +82,15 @@ theMap = A.attrMap
   , (A.attrName "bold" , V.withStyle (fg V.white) V.bold)
   ]
 
+initialState :: [Branch] -> L.List () Branch
+initialState branches = L.list () (Vec.fromList branches) 1
+
 theApp :: M.App (L.List () Branch) e ()
 theApp = M.App { M.appDraw         = drawUI
                , M.appChooseCursor = M.showFirstCursor
                , M.appHandleEvent  = appEvent
                , M.appStartEvent   = return
-               , M.appAttrMap      = const theMap
+               , M.appAttrMap      = const attributeMap
                }
 
 main :: IO ()
@@ -107,8 +103,7 @@ main = do
           Just branch -> Git.checkout branch
           Nothing     -> pure $ Left "No branch selected."
         )
+ where
+  printResult (Left  e  ) = putStr e
+  printResult (Right msg) = putStr msg
 
-
-printResult :: Either String String -> IO ()
-printResult (Left  e  ) = putStr e
-printResult (Right msg) = putStr msg
