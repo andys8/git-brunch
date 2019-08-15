17d65bb11725d4a251f9d816118ca18dc524d912
andys8 <andys8@users.noreply.github.com>
2019-08-15 14:28:46 +0200

    Can jump between local and remote list

--- src/GitBrunch.hs
+++ src/GitBrunch.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module GitBrunch where
 
 
@@ -39,37 +40,38 @@ import           Data.List
 import           Data.Char
 
 data Name = Local | Remote deriving (Ord, Eq, Show)
-data State = State { localBranches :: L.List Name Branch, remoteBranches :: L.List Name Branch }
+data State = State { focus :: Name, localBranches :: L.List Name Branch, remoteBranches :: L.List Name Branch }
 
 drawUI :: State -> [Widget Name]
 drawUI state =
-  [ C.vCenter
-      $ padAll 1
-      $ vBox
-          [ hBox [C.hCenter branchBox1, C.hCenter branchBox2]
-          , str " "
-          , instructions
-          ]
+  [ C.vCenter $ padAll 1 $ vBox
+      [ hBox [C.hCenter localBranchList, C.hCenter remoteBranchList]
+      , str " "
+      , instructions
+      ]
   ]
  where
-  branchBox1   = drawBranchList $ localBranches state
-  branchBox2   = drawBranchList $ remoteBranches state
+  hasFocus l = L.listName l == focus state
+  localBranchList =
+    drawBranchList (hasFocus $ localBranches state) (localBranches state)
+  remoteBranchList =
+    drawBranchList (hasFocus $ remoteBranches state) (remoteBranches state)
   instructions = vBox
     [ drawInstruction "HJKL/arrows" "navigate"
     , drawInstruction "Enter"       "checkout"
     , drawInstruction "Esc/Q"       "exit"
     ]
 
-drawBranchList :: Show a => L.List Name a -> Widget Name
-drawBranchList list =
+drawBranchList :: Show a => Bool -> L.List Name a -> Widget Name
+drawBranchList hasFocus list =
   withBorderStyle BS.unicodeBold
-    $ B.borderWithLabel (withAttr "bold" $ str (title (L.listName list)))
+    $ B.borderWithLabel (drawTitle list)
     $ hLimit 80
     $ L.renderList listDrawElement hasFocus list
  where
-  hasFocus = L.listName list == Local
   title Local  = map toUpper "local"
   title Remote = map toUpper "remote"
+  drawTitle = withAttr "bold" . str . title . L.listName
 
 listDrawElement :: Show a => Bool -> a -> Widget Name
 listDrawElement selected a = padLeft (T.Pad 1) $ padRight T.Max $ str (show a)
@@ -83,16 +85,30 @@ drawInstruction keys action =
     <+> str "."
 
 appEvent :: State -> T.BrickEvent Name e -> T.EventM Name (T.Next State)
-appEvent state (T.VtyEvent e) = case e of
-  V.EvKey V.KEsc [] ->
-    M.halt $ state { localBranches = L.listClear $ localBranches state }
-  V.EvKey (V.KChar 'q') [] ->
-    M.halt $ state { localBranches = L.listClear $ localBranches state }
+appEvent state (T.VtyEvent e) =
+  let
+    checkoutBranch = M.halt state
+    focusLocal     = M.continue $ state { focus = Local }
+    focusRemote    = M.continue $ state { focus = Remote }
 
-  V.EvKey V.KEnter [] -> M.halt state
-  ev ->
-    (M.continue . (\l -> state { localBranches = l }))
-      =<< L.handleListEventVi L.handleListEvent ev (localBranches state)
+    quit = M.halt $ state { localBranches = L.listClear $ localBranches state }
+    navigateDefault event = case focus state of
+      Local ->
+        (M.continue . (\l -> state { localBranches = l }))
+          =<< L.handleListEventVi L.handleListEvent event (localBranches state)
+      Remote ->
+        (M.continue . (\l -> state { remoteBranches = l }))
+          =<< L.handleListEventVi L.handleListEvent event (remoteBranches state)
+  in
+    case e of
+      V.EvKey V.KEsc        [] -> quit
+      V.EvKey (V.KChar 'q') [] -> quit
+      V.EvKey V.KEnter      [] -> checkoutBranch
+      V.EvKey V.KLeft       [] -> focusLocal
+      V.EvKey (V.KChar 'h') [] -> focusLocal
+      V.EvKey V.KRight      [] -> focusRemote
+      V.EvKey (V.KChar 'l') [] -> focusRemote
+      event                    -> navigateDefault event
 appEvent state _ = M.continue state
 
 
@@ -108,7 +124,8 @@ attributeMap = A.attrMap
 
 initialState :: [Branch] -> State
 initialState branches = State
-  { localBranches  = L.list Local (Vec.fromList local) 1
+  { focus          = Local
+  , localBranches  = L.list Local (Vec.fromList local) 1
   , remoteBranches = L.list Remote (Vec.fromList remote) 1
   }
  where
@@ -123,14 +140,22 @@ theApp = M.App { M.appDraw         = drawUI
                , M.appStartEvent   = return
                , M.appAttrMap      = const attributeMap
                }
+stateToBranch :: State -> Maybe Branch
+stateToBranch State { focus, localBranches, remoteBranches } = snd
+  <$> L.listSelectedElement bs
+ where
+  bs = case focus of
+    Local  -> localBranches
+    Remote -> remoteBranches
+
+
 
 main :: IO ()
 main = do
   branches   <- Git.listBranches
   finalState <- M.defaultMain theApp (initialState branches)
-  let branch = snd <$> L.listSelectedElement (localBranches finalState)
   printResult
-    =<< (case branch of
+    =<< (case stateToBranch finalState of
           Just branch -> Git.checkout branch
           Nothing     -> pure $ Left "No branch selected."
         )
