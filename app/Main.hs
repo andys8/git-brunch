eada34940094fe617bda00327fa9d20ce1a39456
andys8 <andys8@users.noreply.github.com>
2020-04-03 15:46:26 +0200

    CLI flags: --version and --help

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,29 @@
 module Main where
 
+import           Options.Applicative
+import           Paths_git_brunch               ( version )
+import           Data.Version                   ( showVersion )
+
 import qualified GitBrunch
 
+
+data Mode = RunGitBrunch | ShowVersion
+
+
 main :: IO ()
-main = GitBrunch.main
+main = run =<< execParser opts
+ where
+  opts = info
+    (versionParser <|> pure RunGitBrunch <**> helper)
+    (header "git-brunch - A git checkout and rebase command-line tool")
+
+
+run :: Mode -> IO ()
+run ShowVersion  = putStrLn $ showVersion version
+run RunGitBrunch = GitBrunch.main
+
+
+versionParser :: Parser Mode
+versionParser =
+  flag' ShowVersion (long "version" <> short 'v' <> help "Show version")
 
