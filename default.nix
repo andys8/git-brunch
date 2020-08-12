814029d5ae46b8da3da97c9ad9eec444dc40e344
andys8 <andys8@users.noreply.github.com>
2020-08-12 23:09:04 +0200

    Bump version to v1.4.0.0

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+{ nixpkgs ? import <nixpkgs> { }, compiler ? "default", doBenchmark ? false }:
 let
   inherit (nixpkgs) pkgs;
 
@@ -15,45 +15,47 @@ let
     , vector
     , vty
     }:
-      mkDerivation {
-        pname = "git-brunch";
-        version = "1.3.1.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        libraryToolDepends = [ hpack ];
-        executableHaskellDepends = [
-          base
-          brick
-          hspec
-          microlens
-          optparse-applicative
-          process
-          vector
-          vty
-        ];
-        testHaskellDepends = [
-          base
-          brick
-          hspec
-          microlens
-          optparse-applicative
-          process
-          vector
-          vty
-        ];
-        prePatch = "hpack";
-        homepage = "https://github.com/andys8/git-brunch#readme";
-        description = "git checkout command-line tool";
-        license = stdenv.lib.licenses.bsd3;
-      };
+    mkDerivation {
+      pname = "git-brunch";
+      version = "1.4.0.0";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      libraryToolDepends = [ hpack ];
+      executableHaskellDepends = [
+        base
+        brick
+        hspec
+        microlens
+        optparse-applicative
+        process
+        vector
+        vty
+      ];
+      testHaskellDepends = [
+        base
+        brick
+        hspec
+        microlens
+        optparse-applicative
+        process
+        vector
+        vty
+      ];
+      prePatch = "hpack";
+      homepage = "https://github.com/andys8/git-brunch#readme";
+      description = "git checkout command-line tool";
+      license = stdenv.lib.licenses.bsd3;
+    };
 
-  haskellPackages = if compiler == "default"
-  then pkgs.haskellPackages
-  else pkgs.haskell.packages.${compiler};
+  haskellPackages =
+    if compiler == "default"
+    then pkgs.haskellPackages
+    else pkgs.haskell.packages.${compiler};
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (haskellPackages.callPackage f { });
+
 in
 if pkgs.lib.inNixShell then drv.env else drv
