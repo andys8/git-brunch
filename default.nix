1f262ed40e13c9609b6621e6fff636fee298aadb
andys8 <andys8@users.noreply.github.com>
2022-12-28 17:23:31 +0100

    v1.6.0

--- default.nix
+++ default.nix
@@ -1,76 +1,42 @@
-{ nixpkgs ? import <nixpkgs> { }, compiler ? "default", doBenchmark ? false }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f =
-    { mkDerivation
-    , base
-    , brick
-    , extra
-    , hpack
-    , hspec
-    , lib
-    , microlens
-    , microlens-mtl
-    , mtl
-    , optparse-applicative
-    , process
-    , text
-    , vector
-    , vty
-    }:
-    mkDerivation {
-      pname = "git-brunch";
-      version = "1.5.1.0";
-      src = ./.;
-      isLibrary = false;
-      isExecutable = true;
-      libraryToolDepends = [ hpack ];
-      executableHaskellDepends = [
-        base
-        brick
-        extra
-        hspec
-        microlens
-        microlens-mtl
-        mtl
-        optparse-applicative
-        process
-        text
-        vector
-        vty
-      ];
-      testHaskellDepends = [
-        base
-        brick
-        extra
-        hspec
-        microlens
-        microlens-mtl
-        mtl
-        optparse-applicative
-        process
-        text
-        vector
-        vty
-      ];
-      prePatch = "hpack";
-      homepage = "https://github.com/andys8/git-brunch#readme";
-      description = "git checkout command-line tool";
-      license = lib.licenses.bsd3;
-    };
+  f = { mkDerivation, base, brick, extra, hpack, hspec, lib
+      , microlens, microlens-mtl, mtl, optparse-applicative, process
+      , text, vector, vty
+      }:
+      mkDerivation {
+        pname = "git-brunch";
+        version = "1.6.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        libraryToolDepends = [ hpack ];
+        executableHaskellDepends = [
+          base brick extra hspec microlens microlens-mtl mtl
+          optparse-applicative process text vector vty
+        ];
+        testHaskellDepends = [
+          base brick extra hspec microlens microlens-mtl mtl
+          optparse-applicative process text vector vty
+        ];
+        prePatch = "hpack";
+        homepage = "https://github.com/andys8/git-brunch#readme";
+        description = "git checkout command-line tool";
+        license = lib.licenses.bsd3;
+      };
 
-  haskellPackages =
-    if compiler == "default"
-    then pkgs.haskellPackages
-    else pkgs.haskell.packages.${compiler};
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f { });
+  drv = variant (haskellPackages.callPackage f {});
 
 in
 
-if pkgs.lib.inNixShell then drv.env else drv
+  if pkgs.lib.inNixShell then drv.env else drv
