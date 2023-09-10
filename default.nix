52c79a06a1ecc68546a3f67789655cd6a68f23c9
andys8 <andys8@users.noreply.github.com>
2023-09-10 13:34:58 +0200

    v1.7.0

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
       }:
       mkDerivation {
         pname = "git-brunch";
-        version = "1.6.0.0";
+        version = "1.7.0.0";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
